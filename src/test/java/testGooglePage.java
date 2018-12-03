bad58d796435df4b835ce0706e458afc52bce0b0
alemiasheuski <35cdWVXL6Ty5ZlphHObo>
2018-12-03 13:03:03 -0800

    add jenkins.file to configure pipeline as a code.

--- src/test/java/testGooglePage.java
+++ src/test/java/testGooglePage.java
@@ -4,12 +4,9 @@ import org.junit.Test;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import java.net.URL;
-import java.nio.file.Paths;
 
 public class testGooglePage {
 
